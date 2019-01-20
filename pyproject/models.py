c80029e126400999561e84a7b70107127f211217
vromanuk <romanuk_vlad@ukr.net>
2019-01-20 20:17:57 +0200

    Changed db structure

--- pyproject/models.py
+++ pyproject/models.py
@@ -7,9 +7,9 @@ Contracts = Table(
         'contracts',
         metadata,
         Column('id', Integer, primary_key=True, autoincrement=True),
-        Column('title', String, nullable=False),
-        Column('price', Float),
-        Column('description', String),
+        Column('title', String, nullable=False, unique=True),
+        Column('price', Float, nullable=False),
+        Column('description', String, nullable=True),
         Column('expiration_date', Date, default=date.today())
         )
 
@@ -17,7 +17,7 @@ Payments = Table(
         'payments',
         metadata,
         Column('id', Integer, primary_key=True, autoincrement=True),
-        Column('contracts_id', None, ForeignKey('contracts.id')),
-        Column('amount', Float),
+        Column('contracts_id', Integer, ForeignKey('contracts.id')),
+        Column('amount', Float, nullable=False),
         Column('date_time', Date, default=datetime.now())
         )
