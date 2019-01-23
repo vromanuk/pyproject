4e4c2869dd64d803c32edd971f3fb721ee51411b
vromanuk <romanuk_vlad@ukr.net>
2019-01-23 22:22:05 +0200

    add limits to model columns

--- pyproject/models.py
+++ pyproject/models.py
@@ -6,15 +6,15 @@ Contracts = Table(
         'contracts',
         metadata,
         Column('id', Integer, primary_key=True),
-        Column('title', String, nullable=False, unique=True),
+        Column('title', String(20), nullable=False, unique=True),
         Column('price', Float, nullable=False),
-        Column('description', String, nullable=True)
+        Column('description', String(100), nullable=True)
         )
 
 Payments = Table(
         'payments',
         metadata,
         Column('id', Integer, primary_key=True),
-        Column('contracts_id', Integer, ForeignKey('contracts.id')),
+        Column('contracts_id', Integer, ForeignKey('contracts.id'), nullable=False),
         Column('amount', Float, nullable=False)
         )
