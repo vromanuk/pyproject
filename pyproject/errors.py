c9289643da0c6b97f8dbb2962056562878944cf3
vromanuk <romanuk_vlad@ukr.net>
2019-01-24 23:28:24 +0200

    refactore code

--- pyproject/errors.py
+++ pyproject/errors.py
@@ -6,17 +6,17 @@ from pyproject import app
 
 @app.errorhandler(ValidationError)
 def validation_error(error):
-    app.logger.info(f'{error.message} {error.status_code}'
-    return make_response({'Message': error.message}, error.status_code)
+    app.logger.info(f'{error.message} {error.status_code}')
+    return make_response({"Message": error.message}, error.status_code)
 
 
 @app.error_handler(404)
 def not_found_error(error):
     app.logger.info(f'{error.message} {error.status_code}')
-    return make_response({'Message': error.message}, error.status_code)
+    return make_response({"Message": error.message}, error.status_code)
 
 
 @app.error_handler(500)
 def not_found_error(error):
-    app.logger.info(f'{error.message} {error.status_code}'
-    return make_response({'Message': error.message}, error.status_code)
+    app.logger.info(f'{error.message} {error.status_code}')
+    return make_response({"Message": error.message}, error.status_code)
